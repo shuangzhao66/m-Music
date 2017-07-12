4eeb81c98d70b8b2ac5b208759410783ab5169af
zs <314496846@qq.com>
2017-07-12 10:53:02 +0800

    recommend页面 轮播图结束，列表渲染完成

---
+++ mMusic/src/components/singer/singer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="singer" ref="singer">
+  	
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  
+  export default {
+  	data() {
+  		return {
+  		}
+  	},
+  	created() {
+  	},
+  	methods: {
+  		
+  	},
+  	components: {
+  	  
+  	}
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
